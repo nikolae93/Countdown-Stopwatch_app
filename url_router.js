c251f3cfa22b74f828bb37897f28e3fd5d6cb57a
Nikola Ekmedzic <nikola.ekmedzic@direkcija.rzzo.sr.gov.yu>
2024-10-02 09:35:13 +0200

    Add basic router JS file, test

---
+++ url_router.js
@@ -0,0 +1,60 @@
+console.log("Router connected");
+
+document.addEventListener("click",(e)=>{
+   const {target} = e;
+   if(!target.matches("header div a")){
+    return;
+   }
+    e.preventDefault();
+    // console.log("Kliki kliki");
+    urlRoute();
+});
+
+const urlRoutes = {
+    "404":{
+        template:"/templates/404.html",
+        title:"",
+        description:""
+    },
+    "/":{
+        template:"/templates/index.html",
+        title:"",
+        description:""
+    },
+    "/test":{
+        template:"/templates/test.html",
+        title:"",
+        description:""
+    },
+    "/test2":{
+        template:"/templates/test2.html",
+        title:"",
+        description:""
+    }
+
+}
+
+function urlRoute(event){
+      event = event || window.event;
+      event.preventDefault();
+      // 
+      window.history.pushState({},"",event.target.href);
+      urlLocationHandler();
+}
+
+const  urlLocationHandler= async()=>{
+    const location = window.location.pathname;
+    if(location.length===0){
+        location='/';
+    }
+
+    const route = urlRoutes[location] || urlRoutes["404"];
+    const html = await fetch(route.template).then((res)=>res.text());
+
+    document.getElementById("content").innerHTML=html;
+}
+
+window.onpopstate= urlLocationHandler;
+window.route=urlRoute;
+
+urlLocationHandler();
